7324b732a8e384718e13a727c6f75c097427b881
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-09-03 15:41:17 -0400

    Moved code into a container class

--- gui.py
+++ gui.py
@@ -1,17 +1,37 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 # Needed for access to command line arguments
 import sys
 
+class GUIWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Change the title of the window
+        self.setWindowTitle("Forecast Tool")
+
+        # Change the window icon
+        self.setWindowIcon(QIcon('Forecast Tool Logo.png'))
+
+        # Resize the window
+        self.resize(1000,750)
+
+        # Change the background colour
+        self.setStyleSheet("background-color: #133C55;")
+
+
+        # Windows are hidden by default, must run this command AT END to show!
+        self.show()
+
+
 # Allows app to utilize command line arguments
 app = QApplication(sys.argv)
 
-# Create a QWidget which is the window
-window = QWidget()
-window.show() # Windows are hidden by default
+# Create an instance of the GUI Window 
+window = GUIWindow()
 
-#Change the title of the window
-window.setWindowTitle("Forecast Tool")
+# Start the app, return an actual exit code
+sys.exit(app.exec())
 
-#Start the event loop
-app.exec()
+
