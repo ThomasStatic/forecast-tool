7927985326cee0c57a7e7cd525001449826ae3b9
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-09-03 15:05:39 -0400

    Created window for GUI and a window logo

--- gui.py
+++ gui.py
@@ -1,2 +1,17 @@
-import tkinter as tk
+from PyQt5.QtWidgets import QApplication, QWidget
 
+# Needed for access to command line arguments
+import sys
+
+# Allows app to utilize command line arguments
+app = QApplication(sys.argv)
+
+# Create a QWidget which is the window
+window = QWidget()
+window.show() # Windows are hidden by default
+
+#Change the title of the window
+window.setWindowTitle("Forecast Tool")
+
+#Start the event loop
+app.exec()
