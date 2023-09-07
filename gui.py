dd99c8877819e62e4cca83a65a4a17d2312c3ea0
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-09-07 13:10:30 -0400

    Created the first textbox that is visible conditionally dependent on radio button

--- gui.py
+++ gui.py
@@ -95,11 +95,17 @@ class GUIWindow(QMainWindow):
         # Create the radio buttons
         zeroIndepVarRB = QRadioButton("0", self)
         zeroIndepVarRB.setChecked(True) # Set the default value to 0 independent variables
+        zeroIndepVarRB.numVars = 0
         oneIndepVarRB = QRadioButton("1", self)
+        oneIndepVarRB.numVars = 1
         twoIndepVarRB = QRadioButton("2", self)
+        twoIndepVarRB.numVars = 2
         threeIndepVarRB = QRadioButton("3", self)
+        threeIndepVarRB.numVars = 3
         fourIndepVarRB = QRadioButton("4", self)
+        fourIndepVarRB.numVars = 4
         fiveIndepVarRB = QRadioButton("5", self)
+        fiveIndepVarRB.numVars = 5
 
         # Add the radio buttons to the same group
         indepVarButtonGroup.addButton(zeroIndepVarRB)
@@ -124,6 +130,22 @@ class GUIWindow(QMainWindow):
         threeIndepVarRB.setStyleSheet("color: #36F1CD")
         fourIndepVarRB.setStyleSheet("color: #36F1CD")
         fiveIndepVarRB.setStyleSheet("color: #36F1CD")
+
+        # Connect the radiobuttons to their method
+        zeroIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+        oneIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+        twoIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+        threeIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+        fourIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+        fiveIndepVarRB.toggled.connect(self.radioButtonOnClicked)
+
+        # Create the text box for indep var 1 column
+        self.indepVar1Textbox = QLineEdit(self)
+        self.indepVar1Textbox.setFont(QFont("Arial", 15))
+        self.indepVar1Textbox.setStyleSheet("background-color: white;")
+        self.indepVar1Textbox.resize(100, 35)
+        self.indepVar1Textbox.move(100, 600)
+        self.indepVar1Textbox.hide()
         
 
 
@@ -135,6 +157,23 @@ class GUIWindow(QMainWindow):
     def upload_button_click(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File', '*.xlsx')
         print('Path file :', filename)
+
+    def radioButtonOnClicked(self):
+        """Create a function to display the appropriate number of text fields depending on what radio button is clicked"""
+        
+        # Identify what radiobutton was clicked
+        radioButton = self.sender()
+        if radioButton.isChecked():
+
+            # Add in conditional statements to hide text fields depending on what radiobutton was selected
+            print(radioButton.numVars) # Place holder code for testing buttons
+
+            if(radioButton.numVars == 0):
+                self.indepVar1Textbox.hide()
+
+            elif(radioButton.numVars == 1):
+                self.indepVar1Textbox.show()
+
 
 
 # Allows app to utilize command line arguments
