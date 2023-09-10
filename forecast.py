0a101a5231dd9e11b2217dc4b36453db26c75efa
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-09-10 17:02:16 -0400

    Moved forecast code to its own method

--- forecast.py
+++ forecast.py
@@ -11,14 +11,18 @@ class ForecastTool:
         # Convert data to a pandas dataframe
         self.df = pd.read_excel(filepath) 
 
+        # Set the date column from the data set to be a DatetimeIndex
         idx = pd.DatetimeIndex(self.df[dateCol])
-
         self.df = self.df.set_index(idx)
         self.df = self.df.drop(dateCol, axis=1)
 
+        
 
+    def run_forecast_unbiased(self):
+        """Run AutoTS forecast on the data and return an unbiased set of forecast results"""
 
-        self.model = AutoTS(
+        # Create a autoTS model
+        model = AutoTS(
             forecast_length=12, 
             frequency='infer',
             ensemble='auto',
@@ -26,16 +30,23 @@ class ForecastTool:
             num_validations=2,
         )
 
+        # Fit the data set to the model
+        model = model.fit(self.df)
 
-        self.model = self.model.fit(self.df)
+        # Run predictions on the data
+        prediction = model.predict()
 
-        self.prediction = self.model.predict()    
+        # Create a dataframe of unbiased prediction results
+        forecasts_df = prediction.forecast
 
-        self.forecasts_df = self.prediction.forecast
+        #forecasts_up = prediction.upper_forecast
 
-        self.forecasts_up = self.prediction.upper_forecast
+        #forecasts_down = prediction.lower_forecast
 
-        self.forecasts_down = self.prediction.lower_forecast
+        return forecasts_df
+
+
+    
 
 
 test = ForecastTool(filepath="C:\\Users\Thomas\\Downloads\\forecast-tool\\testDataZeroIndepVars.xlsx", dateCol="Date")
